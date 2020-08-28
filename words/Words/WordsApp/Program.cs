477b6e801cee30e73c1d9c2e0d38a9da7b95be32
Daniel Zoné <dannezone@gmail.com>
2020-08-28 17:34:43 +0200

    Finished WordsApp

---
+++ words/Words/WordsApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // Skriv en konsollapplikation som tar emot en skriven text.
+            // Ha med antal ord
+            // Antal vokaler
+            // Vilket det längsta ordet är
+            Console.WriteLine("Insert a sentence");
+            char[] vowels = new char[] { 'a', 'o', 'i', 'e', 'u', 'y', 'å', 'ä', 'ö' };
+
+            string userInput = Console.ReadLine();
+            string myLowerCaseString = userInput.ToLower();
+            
+            int wordcount = userInput.Length;
+
+            string[] words = myLowerCaseString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int vowelCount = 0;
+            string longestWord = string.Empty;
+            
+            foreach (var word in words)
+            {
+                foreach (var character in word)
+                {
+                    if (vowels.Contains(character))
+                    {
+                        vowelCount++;
+                    }
+                    if (word.Length > longestWord.Length)
+                    {
+                        longestWord = word;
+                    }
+                }
+            }
+            
+            Console.WriteLine("Longest word: " + longestWord);
+            Console.WriteLine("Vowel count: " + vowelCount);
+            Console.WriteLine("Word count: " + wordcount);
+        }
+    }
+}
