c8435594578523c59c409e75dbc96d36f584e861
Edgar Runnman <edgar.runnman@gmail.com>
2020-08-28 14:59:57 +0200

    chess app running

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] SignArray = new char[] {'░', '▓' };
+            for(int i = 0; i <= 4; i++)
+            {
+                for (int y = 0; y< 8; y++)
+                {
+                    int signIndex = y % 2;
+                    RenderSign(SignArray[signIndex]);
+                }
+                Console.Write("\n");
+                for (int y = 1; y < 9; y++)
+                {
+                    int signIndex = y % 2;
+                    RenderSign(SignArray[signIndex]);
+                }
+                Console.Write("\n");
+            }
+        static void RenderSign(char sign)
+            {
+                Console.Write(sign);
+                Console.Write(sign);
+                Console.Write(sign);
+            }
+        }
+    }
+}
