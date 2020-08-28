d0958a23fb6621532dea7ced3b915d902cf4605c
Daniel Zoné <dannezone@gmail.com>
2020-08-28 15:20:09 +0200

    Chess app

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 16; x++)
+                {
+                    var characterIndex = (x / 2 + y) % 2;
+                    var character = characterIndex == 0 ? '▓' : '░';
+                    Console.Write(character);
+                }
+                Console.Write('\n');
+            }
+
+            #region Mitt Försök
+            /*
+            for (int i = 1; i < 65; i++)
+            {
+                int lineBreak = i % 8;
+                int test = i % 2;
+                int lineSwitch = 0;
+
+                if (lineSwitch % 2 == 1)
+                {
+                    if (lineBreak == 1)
+                    {
+                        Console.Write("\n");
+                        lineSwitch++;
+                    }
+                    if (test == 1)
+                        Console.Write('▓');
+                    else
+                        Console.Write('░');
+                }
+                else //if (lineSwitch % 2 == 1)
+                {
+                    if (lineBreak == 1)
+                    {
+                        Console.Write("\n");
+                        lineSwitch++;
+                    }
+                    if (test == 0)
+                        Console.Write('▓');
+                    else
+                        Console.Write('░');
+                }
+            }
+            */
+            #endregion
+        }
+    }
+}
