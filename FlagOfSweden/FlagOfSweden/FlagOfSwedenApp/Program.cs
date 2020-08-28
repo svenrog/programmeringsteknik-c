3cb72f769906a010b2af270d4920b34cd1cd7b86
Edgar Runnman <edgar.runnman@gmail.com>
2020-08-28 15:20:20 +0200

    FlagOfSweden running

---
+++ FlagOfSweden/FlagOfSweden/FlagOfSwedenApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FlagOfSwedenApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                BlueYellowLineRender();
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                YellowLineRender();
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                BlueYellowLineRender();
+            }
+
+        }
+
+        static void BlueYellowLineRender()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(" ");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Write(" ");
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.Write(" ");
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("\n");
+        }
+
+        static void YellowLineRender()
+        {
+
+            for (int i = 0; i < 35; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.Write(" ");
+            }
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("\n");
+
+        }
+    }
+}
