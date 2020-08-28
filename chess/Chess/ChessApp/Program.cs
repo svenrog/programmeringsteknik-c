5dd4be9cedb8e6d5446e3855cae2ac9f1dd860c0
TooOldForThisGit <j.o.andersson@hotmail.se>
2020-08-28 16:03:14 +0200

    Commit chess and a half flag and som words/Jens

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char white = '░';
+            char black = '▓';
+            //Console.WriteLine(white);
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((i % 2) == 0)
+                    {
+                        Console.Write(black);
+                        Console.Write(white);
+                    }
+                    else
+                    {
+                        Console.Write(white);
+                        Console.Write(black);
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();/*
+            for (int i = 0; i < 8; i++)
+            {
+                if (i != 5 && i != 6)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        if (j != 5 && j != 6)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.Write(black);
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.Write(white);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    for (int k = 0; k < 8; k++)
+                        Console.WriteLine(white);
+                    for (int m = 0; m< 8; m++)
+                        Console.WriteLine(white);
+                }
+                    
+                
+                Console.WriteLine();
+            }
+            Console.ReadLine();*/
+        }
+    }
+}
---
+++ words/Words/WordsApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skriv en konsolapplikation som tar emot en skriven text.
+            int wordCount;
+            int vowelCount = 0;
+            string longestWord;
+            
+
+            var vowel = new char[] { 'a', 'i', 'e', 'o', 'u', 'y', 'å', 'ä', 'ö' };
+            Console.WriteLine("Var vänlig och skriv in en text: ");
+            //string input = Console.ReadLine();
+            var inputStringArray = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);//new [] för FrameWork 
+
+            //Vi vill ha ut följande:
+            //Antal ord
+            Console.WriteLine("Antal ord: " + inputStringArray.Length);
+            //Antal vokaler
+            foreach (var vowels in inputStringArray)
+                foreach (var characters in vowel)
+                    if (vowels.Contains(characters)) ++vowelCount;
+            Console.WriteLine("Antal vokaler: " + vowelCount);
+            //Vilket är det längsta ordet
+            var sorted = inputStringArray.OrderBy(n => n.Length);
+            longestWord = sorted.LastOrDefault();
+
+            Console.WriteLine("Längsta ordet: " + inputStringArray.Max());
+            Console.ReadKey();
+        }
+    }
+}
