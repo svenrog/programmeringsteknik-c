d1e62a0f6e17715ba1b8e4572445a9378580e025
PROGRAMMERINGEN\Patrik <patrikpersson45@gmail.com>
2020-08-28 15:20:29 +0200

    Failed program

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    int intLinebreake = i % 8;
+            //    int intTest = i % 2;
+            //    int intLineswitch = 1;
+
+            //    if (intLineswitch % 2 == 1)
+            //    {
+            //        intLineswitch++;
+
+            //        if (intTest == 1)
+            //        {
+            //            Console.Write("░");
+            //        }
+            //        else
+            //        {
+            //            Console.Write("▓");
+            //        }
+            //    }
+            //    if (intLineswitch % 2 == 1)
+            //    {
+            //        if (intLineswitch % 2 == 1)
+            //        {
+            //            intLineswitch++;
+
+            //            if (intTest == 1)
+            //            {
+            //                Console.Write("░");
+            //            }
+            //            else
+            //            {
+            //                Console.Write("▓");
+            //            }
+            //            intLineswitch++;
+
+            //            if (intTest == 1)
+            //            {
+            //                Console.Write("░");
+            //            }
+            //            else
+            //            {
+            //                Console.Write("▓");
+            //            }
+
+            //        }
+
+
+
+            //        Console.ReadKey(true);
+
+            //-----------------------------------------------------------------------------------------
+
+            // Lösning!
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 16; x++)
+                {
+                    int characterIndex = (x / 2 + y) % 2;
+                    char character = characterIndex == 0 ? '░' : '▓';
+
+                    Console.Write(character);
+                }
+                Console.Write('\n');
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
