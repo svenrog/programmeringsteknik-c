281f4e5ecd25b4e8756f73dc97148a460fc4f897
Sven-Erik Jonsson <sven.erik@geta.no>
2020-08-28 15:20:09 +0200

    Chess app

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var y = 0; y < 8; y++)
+            {
+                for (var x = 0; x < 16; x++)
+                {
+                    int characterIndex = (x / 2 + y) % 2;
+                    char character = characterIndex == 0 ? '░' : '▓';
+
+                    Console.Write(character);
+                }
+
+                Console.Write('\n');
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
