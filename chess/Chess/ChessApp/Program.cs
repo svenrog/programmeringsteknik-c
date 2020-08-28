8c8f54733ccca13e25ad89a3eb35b5784d2e62a4
hampussle <hlinderstam@gmail.com>
2020-08-28 15:22:23 +0200

    ChessApp

---
+++ chess/Chess/ChessApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //░ och ▓)
+            int j = 0;
+            for (var i = 0; i < 64; i++)
+            {
+                if(i % 8 == 0)
+                {
+                    Console.WriteLine("");
+                    j++;
+                }
+                if (i % 2 == 0)
+                {
+                    if(j % 2 == 1)
+                    {
+                        Console.Write("▓▓");
+                        continue;
+                    }
+                    Console.Write("░░");
+                } else if(i % 2 == 1 && j % 2 == 0)
+                {
+                    Console.Write("▓▓");
+                } else
+                {
+                    Console.Write("░░");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
