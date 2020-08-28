197348c8139cfe128e2c639000cc9e1b840e16ff
PROGRAMMERINGEN\Patrik <patrikpersson45@gmail.com>
2020-08-28 16:01:27 +0200

    Not finished.

---
+++ Words/Words/WordsApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace WordsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skriv ut en konsolapplikation som tar emot en skriven text.
+
+            // Antal ord
+            // Antal vokaler
+            // vilket är det längsta ordet
+
+            // Word count
+            // Vowel count
+            // Longest word
+
+            int intVowelCount = 0;
+            int intWordCount = 0;
+            char[] charVowels = new char[] { 'a', 'o', 'i', 'e', 'u', 'y', 'å', 'ä', 'ö' };
+            string longestWord = string.Empty;
+
+            Console.WriteLine("Write a sentence please.");
+            string enteredString = Console.ReadLine();                        
+            string lowercaseString = enteredString.ToLower();
+
+            string[] words = enteredString.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                foreach (var character in enteredString)
+                {
+                    if (charVowels.Contains(character));
+                    {
+                        intVowelCount++;
+                    }
+                }
+
+                if (word.Length > longestWord.Length)
+                {
+
+                }
+            }
+
+            Console.WriteLine($"Word count is: {intWordCount}");
+            Console.WriteLine($"Vowel count is: {intVowelCount}");
+            Console.WriteLine($"Longest word is: {intWordCount}");
+
+            Console.ReadLine();
+        }
+    }
+}
