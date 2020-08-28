175dc955e304f4f9c309d66d13159157028a85e1
Andreas Brunnström <best_i25@hotmail.com>
2020-08-28 15:20:11 +0200

    Chess app

---
+++ chess/ChessApp/ChessApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChessApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int y = 0; y < 8; y++)
+            {               
+                for(int x = 0; x < 16; x++)
+                {
+                    var characterIndex = (x / 2 + y) % 2;
+
+                    var character = characterIndex == 0 ? '░' : '▓';
+                    Console.Write(character);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
