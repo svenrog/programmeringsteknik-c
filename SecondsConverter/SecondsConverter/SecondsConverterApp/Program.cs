77c54bc770e05dd88965aa90f1f10c10eaf54385
Edgar Runnman <edgar.runnman@gmail.com>
2020-08-29 22:16:12 +0200

    SecondsConverterApp and WordAndLetterOrderChengerApp from session2

---
+++ SecondsConverter/SecondsConverter/SecondsConverterApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SecondsConverterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunder = 1_262_332_654;
+            int ejheladagar = sekunder % 86400;
+            int dagar = (sekunder - ejheladagar) / 86400;
+            int ejhelatimmar = ejheladagar % 3600;
+            int timmar = (ejheladagar - ejhelatimmar) / 3600;
+            int ejhelaminuter = ejhelatimmar % 60;
+            int minuter = (ejhelatimmar - ejhelaminuter) / 60;
+            int secunder = ejhelaminuter;
+            Console.WriteLine($"{sekunder} sekunder är {dagar} dagar, {timmar} timmar, {minuter} minuter, {secunder} sekunder");
+        }
+    }
+}
---
+++ WordAndLetterOrderChenger/WordAndLetterOrderChenger/WordAndLetterOrderChengerApp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WordAndLetterOrderChengerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Skirv in fre text och se vad som händer!");
+                string input = Console.ReadLine();
+                int inputLength = input.Length;
+                Console.WriteLine("\nHär är din text där ordordning och bokstaver är baklänges:");
+                for (int i = 0; i < inputLength; i++)
+                {
+                    int index = inputLength - 1 - i;
+                    Console.Write(input[index]);
+                }
+                Console.WriteLine("\n\nHär är din text där bara ordordning är baklänges:");
+                input += " ";
+                List<string> StringList = new List<string>();
+                string word = "";
+                foreach (char character in input)
+                {
+                    if (character == ' ')
+                    {
+                        StringList.Add(word);
+                        word = "";
+                    }
+                    else
+                    {
+                        word += character;
+                    }
+                }
+                int stringLengts = StringList.Count;
+                for (int i = 0; i < stringLengts; i++)
+                {
+                    int index = stringLengts - 1 - i;
+                    //Console.WriteLine(index);
+                    Console.Write(StringList[index] + " ");
+                }
+                Console.WriteLine("\n\nProva igen!");
+            }
+        }
+    }
+}
+
