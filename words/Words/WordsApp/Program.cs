58bc11c65f62e8e6e4e834e6c23974caab75f8d6
Magnusson <Magnusson-5@hotmail.com>
2020-08-29 21:17:52 +0200

    My solution to WordsApp

--- words/Words/WordsApp/Program.cs
+++ words/Words/WordsApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 
 namespace WordsApp
 {
@@ -7,30 +9,45 @@ namespace WordsApp
     {
         static void Main(string[] args)
         {
-            //skriv en konsolapplikation som tar emot en skriven text
-            //Antal ord som finns i texten, antal vokaler, vilket är det längsta ordet i texten
-            //Word count
-            //Vowel count
-            //Longest word
-            //char vowels = new char[] {'a', 'e', 'i', 'o','u','y','å','ä','ö'}
-            //string test = "this is a test"
-            //foreach (character in test){
-            //
-            //}
+            //skriv en konsolapplikation som tar emot en text //Writeline
+            //Antal ord som finns i texten //lista, antal vokaler, vilket är det längsta ordet i texten //.length
+            //foreach (character in test)
 
-            Console.WriteLine("Write a text: ");
-            string myText = Console.ReadLine();
-            string myTextToLower = myText.ToLower();
 
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö' };
-            string 
+            //My solution
+            Console.WriteLine("Please write a sentence: ");
+
+            string sentence = Console.ReadLine();
+            string sentenceToLower = sentence.ToLower();
+            
             int wordCounter = 0;
+            int vowelsCounter = 0;
 
-            foreach (var word in myTextToLower)
+            //Counting words
+            char[] split = { ' ', '.', ',', '!', '?', ':', ';' };
+            foreach (var word in sentence.Split(split, StringSplitOptions.RemoveEmptyEntries))
             {
-                Console.WriteLine(word);
                 wordCounter++;
             }
+
+            //Counting vowels
+            for(int i = 0; i < sentenceToLower.Length; i++)
+            {
+                if(sentenceToLower[i] == 'a' || sentenceToLower[i] == 'e' || sentenceToLower[i] == 'i'
+                    || sentenceToLower[i] == 'o' || sentenceToLower[i] == 'u' || sentenceToLower[i] == 'y'
+                        || sentenceToLower[i] == 'å' || sentenceToLower[i] == 'ä' || sentenceToLower[i] == 'ö')
+                {
+                    vowelsCounter++;
+                }
+
+            }
+
+            //Looking for the longest word
+            string[] stringOfWords = sentence.Split(' ');
+            string longestWord = stringOfWords.OrderByDescending(n => n.Length).First();
+
+            Console.WriteLine($"There is {wordCounter} words in the sentence, it contains {vowelsCounter} vowels");
+            Console.WriteLine($"and the longest word is: {longestWord}!");
         }
     }
 }
