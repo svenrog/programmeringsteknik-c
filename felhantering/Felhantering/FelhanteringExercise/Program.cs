4adf65ab9534434289ed47f96d544b578f094c51
DESKTOP-4QQ7EF3\elsam <elsa.matteoni@hotmail.com>
2020-08-31 11:09:59 +0200

    Progress

--- felhantering/Felhantering/FelhanteringExercise/Program.cs
+++ felhantering/Felhantering/FelhanteringExercise/Program.cs
@@ -6,7 +6,25 @@ namespace FelhanteringExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int buildError = "stringVaue"; (angett fel värdetyp, här int och string)
+
+            //för att undvika att fel avslutar körningen måste man hantera evetuela körtidsfel med try catch
+            
+            try
+            {
+                string word = "12a";
+                int integer = int.Parse(word);
+            }
+            catch (Exception ex)
+            
+                // denna del är till för att hantera felet, visa fel för användaten eller att skriva till logg.
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                //körs alltid, är till för att städa upp körningen
+                //körs öven om try-satsen innehåller return;
+            }
         }
     }
 }
