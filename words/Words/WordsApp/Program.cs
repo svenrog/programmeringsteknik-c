0a2676ba11a46950728492e844ffa53e2054fcd5
ASUS-ENTERPRISE\tempo <temporal@live.se>
2020-08-31 08:44:26 +0200

    Added a check to see if the user entered anything.

--- words/Words/WordsApp/Program.cs
+++ words/Words/WordsApp/Program.cs
@@ -19,86 +19,94 @@ namespace WordsApp
             //Longest word
 
             Console.WriteLine("Please enter a sentence or more.");
-            string input = " " + Console.ReadLine();
-            //Console.WriteLine(input);
+            string input = " " + Console.ReadLine();            //Då jag räknar antalet ord genom whitespaces, och input inte kommer skrivas ut, så lägger jag till
+                                                                //ett mellanslag. Det blir dock fel om det inte skrivs in något.
 
-            //Steg ett är att hitta all "whitespace"; mellanslag, för att se hur många olika ord som finns.
-            int[] whitespace = new int[input.Length];
-            for (int i = input.Length; i>0; i--)
+            if (input.Length > 1)                               //Om input är 0 är något fel; om den är 1 så skrevs inget in.
             {
-                if (input.Substring((i-1),1) == " ") 
+
+
+                //Steg ett är att hitta all "whitespace"; mellanslag, för att se hur många olika ord som finns.
+                int[] whitespace = new int[input.Length];
+                for (int i = input.Length; i > 0; i--)
                 {
-                    whitespace[(i-1)] = 1;
-                }
-                else 
-                    whitespace[(i-1)] = 0;
-            }
-            
-            
-            /*for (int i = input.Length; i > 0; i--)
-            {
-                Console.WriteLine(whitespace[(i - 1)]);
-            }*/
-
-
-            //Nu har vi en lista i en array. Alla ettor är mellanslag/whitespace, medan nollor är andra tecken.
-            int wordcount = 0;
-            for (int i = input.Length; i > 0; i--)
-            {
-                if (whitespace[(i-1)] == 1)
-                {
-                    wordcount++;
-                }
-            }
-            if (input.Length == 0) Console.WriteLine("The word count is zero.");
-            else Console.WriteLine("The word count is " + wordcount);
-
-
-            char[] vowels = { 'a', 'o', 'i', 'u', 'y', 'å', 'ä', 'ö', 'e'};
-            int check = 0;
-            int vowelCount = 0;
-            for (int i = input.Length; i > 0; i--)
-            {
-                for (int n = 0; n<9; n++)
-                {
-                    check = vowels[n].CompareTo(Convert.ToChar(input.Substring((i-1),1)));      //ComparesTo ger ett värde som inte är noll om det inte är samma.
-                    if (check == 0)
+                    if (input.Substring((i - 1), 1) == " ")
                     {
-                        vowelCount++;
+                        whitespace[(i - 1)] = 1;
                     }
+                    else
+                        whitespace[(i - 1)] = 0;
                 }
-            }
-            Console.WriteLine("The vowel count is: " + vowelCount);
 
 
-            //genom att räkna hur många nollor i rad som är mest så kan vi se vilket ord som är längst i vår 'whitespace' array.
-            //alternativt kan vi leta efter största hålet mellan whitespaces i 'input'
-            int charCounter = 0;
-            int positionIndex = 0;
-            int currentLargest = 0;
-            int ticker = 0;
-            foreach (int i in whitespace)
-            {
-                if (whitespace[ticker] == 0)
+                /*for (int i = input.Length; i > 0; i--)
                 {
-                    charCounter++;
-                }
-                else 
+                    Console.WriteLine(whitespace[(i - 1)]);
+                }*/
+
+
+                //Nu har vi en lista i en array. Alla ettor är mellanslag/whitespace, medan nollor är andra tecken.
+                int wordcount = 0;
+                for (int i = input.Length; i > 0; i--)
                 {
-                    if (charCounter > currentLargest)
+                    if (whitespace[(i - 1)] == 1)
                     {
-                        currentLargest = Math.Max(charCounter, currentLargest);
-                        charCounter = 0;
-                        positionIndex = ticker - currentLargest;
-                    }
-                    else {
-                        charCounter = 0;
+                        wordcount++;
                     }
                 }
-                ticker++;
-                
+                if (input.Length == 0) Console.WriteLine("The word count is zero.");
+                else Console.WriteLine("The word count is " + wordcount);
+
+
+                char[] vowels = { 'a', 'o', 'i', 'u', 'y', 'å', 'ä', 'ö', 'e' };
+                int check = 0;
+                int vowelCount = 0;
+                for (int i = input.Length; i > 0; i--)
+                {
+                    for (int n = 0; n < 9; n++)
+                    {
+                        check = vowels[n].CompareTo(Convert.ToChar(input.Substring((i - 1), 1)));      //ComparesTo ger ett värde som inte är noll om det inte är samma.
+                        if (check == 0)
+                        {
+                            vowelCount++;
+                        }
+                    }
+                }
+                Console.WriteLine("The vowel count is: " + vowelCount);
+
+
+                //genom att räkna hur många nollor i rad som är mest så kan vi se vilket ord som är längst i vår 'whitespace' array.
+                //alternativt kan vi leta efter största hålet mellan whitespaces i 'input'
+                int charCounter = 0;
+                int positionIndex = 0;
+                int currentLargest = 0;
+                int ticker = 0;
+                foreach (int i in whitespace)
+                {
+                    if (whitespace[ticker] == 0)
+                    {
+                        charCounter++;
+                    }
+                    else
+                    {
+                        if (charCounter > currentLargest)
+                        {
+                            currentLargest = Math.Max(charCounter, currentLargest);
+                            charCounter = 0;
+                            positionIndex = ticker - currentLargest;
+                        }
+                        else
+                        {
+                            charCounter = 0;
+                        }
+                    }
+                    ticker++;
+
+                }
+                Console.WriteLine("The largest word is \'" + input.Substring(positionIndex, currentLargest) + "\' at " + currentLargest + " characters");
+
             }
-            Console.WriteLine("The largest word is \'" + input.Substring(positionIndex, currentLargest) + "\' at " + currentLargest + " characters");
+            else Console.WriteLine("You did not follow instructions.");
 
         }
     }
