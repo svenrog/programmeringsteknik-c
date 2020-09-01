ddf875fa8e7b006fa247d2c1456abe61c9eed3c4
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-01 22:37:10 +0200

    hollydayApp running

--- holiday/Holiday/WhichHolidayApp/Program.cs
+++ holiday/Holiday/WhichHolidayApp/Program.cs
@@ -4,10 +4,81 @@ namespace WhichHolidayApp
 {
     class Program
     {
-        static void Main(string[] args)
+		private static string hollyday;
+		static void Main(string[] args)
         {
-            // Implementera följande flödesschema med metoder och användarinmatning
-            // https://pbs.twimg.com/media/EQup9bwXUAEK5a_?format=jpg&name=large
-        }
-    }
+			// Implementera följande flödesschema med metoder och användarinmatning
+			// https://pbs.twimg.com/media/EQup9bwXUAEK5a_?format=jpg&name=large
+			string[] questionArray = new string[11]
+			{
+				"Äter man sill?",
+				"Dricker man must?",
+				"Är fika viktigt?",
+				"Kollar man TV kl 15:00?",
+				"Vispgrädde?",
+				"Importerat från USA av köpman?",
+				"Sylt till",
+				"Vad köper man?",
+				"Är man bakfull?",
+				"kollar man på Ivanhoe?",
+				"Vet folk orsaken till firandet?"
+			};
+			string[,] answerArray = new string[11, 2]
+			{
+				{null, null},
+				{null, "Midsommar"},
+				{null, null},
+				{"Jul","Påsk"},
+				{null,"Kanebullens dag"},
+				{null,null},
+				{"Våffeldagen","Fettisdagen"},
+				{"Alla hjärtans dag","Halloween"},
+				{null,null},
+				{"Nyårsdagen","Föra maj"},
+				{"Kristihimmelsfärd","Nationaldagen"}
+			};
+			int[,] links = new int[11, 2]
+			{
+				{1,2},
+				{3,-1},
+				{4,5},
+				{-1,-1},
+				{6,-1},
+				{7,8},
+				{-1,-1},
+				{-1,-1},
+				{9,10},
+				{-1,-1},
+				{-1,-1}
+			};
+			int pathIndex = 0;
+			while (true)
+			{
+				Console.WriteLine("Vilken svensk högtid?");
+				while (hollyday == null)
+				{
+					pathIndex = GetAnswer(questionArray, answerArray, links, pathIndex);
+				}
+				Console.WriteLine("Då är det " + hollyday);
+				hollyday = null;
+				pathIndex = 0;
+			}
+		}
+
+		private static int GetAnswer(string[] questionArray, string[,] answerArray, int[,] links, int pathIndex)
+		{
+			Console.WriteLine(questionArray[pathIndex]);
+			string input = Console.ReadLine();
+			input = input.ToLower();
+			if (input == "ja")
+			{
+				hollyday = answerArray[pathIndex, 0];
+				return links[pathIndex, 0];
+			}
+			hollyday = answerArray[pathIndex, 1];
+			return links[pathIndex, 1];
+		}
+	}
+
+
 }
