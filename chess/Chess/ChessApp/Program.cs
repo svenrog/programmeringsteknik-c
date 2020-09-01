ceda800c58fa318fd04f0c2090fe6a43b1f687a2
Andreas Brunnström <best_i25@hotmail.com>
2020-09-01 09:38:26 +0200

    Chess again

--- chess/Chess/ChessApp/Program.cs
+++ chess/Chess/ChessApp/Program.cs
@@ -9,6 +9,27 @@ namespace ChessApp
             // Rita ett schackbräde med hjälp av dessa två tecken ░ ▓.
             // Använd gärna metoder för att lösa problemet.
             // Man behöver använda % (modulo)
+
+            static void Draw()
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    for (int y = 0; y < 8; y++)
+                    {
+                        int rest = (x / 2 + y) % 2;
+                        if (rest == 0)
+                        {
+                            Console.Write("░░");
+                        }
+                        else
+                        {
+                            Console.Write("▓▓");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Draw();
         }
     }
 }
