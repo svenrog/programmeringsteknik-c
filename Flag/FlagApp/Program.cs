7fbbc1dbae0c688af3acb1722e2bc570b200b177
hampussle <hlinderstam@gmail.com>
2020-09-01 16:29:03 +0200

    Variable name change

--- Flag/FlagApp/Program.cs
+++ Flag/FlagApp/Program.cs
@@ -16,21 +16,25 @@ namespace FlagApp
 
             //  Använda % 5,6 för mitten av flaggan??
 
+
             static void DrawFlag()
             {
                 for (var height = 0; height < 10; height++)
                 {
+                    bool yellowHeight = height == 4 || height == 5;
+
                     Console.WriteLine();
                     for (var length = 0; length < 16; length++)
                     {
-                        if (height == 4 || height == 5)
+                        bool yellowLength = length == 5 || length == 6;
+                        if (yellowHeight == true)
                         {
                             Console.BackgroundColor = ConsoleColor.Yellow;
                             Console.Write("  ");
                             continue;
                         }
                         
-                        if (length == 5 || length == 6)
+                        if (yellowLength == true)
                         {
                             Console.BackgroundColor = ConsoleColor.Yellow;
                             Console.Write("  ");
