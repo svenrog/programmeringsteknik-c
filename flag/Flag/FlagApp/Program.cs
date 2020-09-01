a2df2cae54fda9ad8d25db1eb2cec49c173e6b83
Andreas Brunnström <best_i25@hotmail.com>
2020-09-01 10:20:33 +0200

    Version 1.1 Put all into methods. Next step might be to move it into a class.

--- flag/Flag/FlagApp/Program.cs
+++ flag/Flag/FlagApp/Program.cs
@@ -9,50 +9,65 @@ namespace FlagApp
             // Skapa en applikation som ritar ut svenska flaggan
             // Använd metoder för att göra utritningen generell
 
-            static void DrawMix()
-            {
-                for (int x = 0; x < 8; x++)
-                {
-                    for (int y = 0; y < 30; y++)
-                    {                        
-                        if (y < 10)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Blue;
-                            Console.Write("-░░-");
-                        }
-                        else if (y > 10 && y < 16)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.Write("-▓▓-");
-                        }
-                        else
-                        {
-                            Console.BackgroundColor = ConsoleColor.Blue;
-                            Console.Write("-░░-");
-                        }
-                    }
-                    Console.WriteLine();
-                }
-            }
+            DrawFlag();
+        }
 
-            static void DrawSingle()
-            {
-                for (int x = 0; x < 8; x++)
-                {
-                    for (int y = 0; y < 30; y++)
-                    {
-                        if (y < 30)
-                        {
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.Write("-▓▓-");
-                        }
-                    }
-                    Console.WriteLine();
-                }
-            }
+        // Perhaps putting all code below into a class ?
+        static void DrawFlag()
+        {
             DrawMix();
             DrawSingle();
             DrawMix();
+        }
+
+        static void DrawMix()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 30; y++)
+                {
+                    if (y < 8)
+                    {
+                        DrawBlueBit();
+                    }
+                    else if (y > 10 && y < 15)
+                    {
+                        DrawYellowBit();
+                    }
+                    else
+                    {
+                        DrawBlueBit();
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void DrawSingle()
+        {
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 30; y++)
+                {
+                    if (y < 30)
+                    {
+                        DrawYellowBit();
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void DrawBlueBit()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("-░░-");
+        }
+
+        static void DrawYellowBit()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("-░░-");
         }
     }
 }
