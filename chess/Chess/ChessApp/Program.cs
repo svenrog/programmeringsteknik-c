bbc05080c1b1aea327318a3986f302060a16ce03
Sven-Erik Jonsson <sven.erik@geta.no>
2020-09-01 09:00:38 +0200

    Updated assignments

--- chess/Chess/ChessApp/Program.cs
+++ chess/Chess/ChessApp/Program.cs
@@ -6,20 +6,9 @@ namespace ChessApp
     {
         static void Main(string[] args)
         {
-            for (var y = 0; y < 8; y++)
-            {
-                for (var x = 0; x < 16; x++)
-                {
-                    int characterIndex = (x / 2 + y) % 2;
-                    char character = characterIndex == 0 ? '░' : '▓';
-
-                    Console.Write(character);
-                }
-
-                Console.Write('\n');
-            }
-
-            Console.ReadKey();
+            // Rita ett schackbräde med hjälp av dessa två tecken ░ ▓.
+            // Använd gärna metoder för att lösa problemet.
+            // Man behöver använda % (modulo)
         }
     }
 }
