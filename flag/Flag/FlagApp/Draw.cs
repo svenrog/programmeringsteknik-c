7c652d4295406f6a1ee8ace0ce9d168d2a88dc4a
Andreas Brunnström <best_i25@hotmail.com>
2020-09-01 11:20:32 +0200

    Version 1.2 All put into a class.

---
+++ flag/Flag/FlagApp/Draw.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlagApp
+{
+    class Draw
+    {
+        public static void DrawFlag()
+        {
+            DrawMix();
+            DrawSingle();
+            DrawMix();
+        }
+
+        static void DrawMix()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 30; y++)
+                {
+                    if (y < 8)
+                    {
+                        DrawBlueBit();
+                    }
+                    else if (y > 10 && y < 15)
+                    {
+                        DrawYellowBit();
+                    }
+                    else
+                    {
+                        DrawBlueBit();
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void DrawSingle()
+        {
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 30; y++)
+                {
+                    if (y < 30)
+                    {
+                        DrawYellowBit();
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void DrawBlueBit()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Write("-░░-");
+        }
+
+        static void DrawYellowBit()
+        {
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.Write("-░░-");
+        }
+
+    }
+}
--- flag/Flag/FlagApp/Program.cs
+++ flag/Flag/FlagApp/Program.cs
@@ -8,66 +8,7 @@ namespace FlagApp
         {
             // Skapa en applikation som ritar ut svenska flaggan
             // Använd metoder för att göra utritningen generell
-
-            DrawFlag();
-        }
-
-        // Perhaps putting all code below into a class ?
-        static void DrawFlag()
-        {
-            DrawMix();
-            DrawSingle();
-            DrawMix();
-        }
-
-        static void DrawMix()
-        {
-            for (int x = 0; x < 8; x++)
-            {
-                for (int y = 0; y < 30; y++)
-                {
-                    if (y < 8)
-                    {
-                        DrawBlueBit();
-                    }
-                    else if (y > 10 && y < 15)
-                    {
-                        DrawYellowBit();
-                    }
-                    else
-                    {
-                        DrawBlueBit();
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static void DrawSingle()
-        {
-            for (int x = 0; x < 7; x++)
-            {
-                for (int y = 0; y < 30; y++)
-                {
-                    if (y < 30)
-                    {
-                        DrawYellowBit();
-                    }
-                }
-                Console.WriteLine();
-            }
-        }
-
-        static void DrawBlueBit()
-        {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.Write("-░░-");
-        }
-
-        static void DrawYellowBit()
-        {
-            Console.BackgroundColor = ConsoleColor.Yellow;
-            Console.Write("-░░-");
+            Draw.DrawFlag();
         }
     }
 }
