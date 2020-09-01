47cd632d302ba1cf0af9d5daeea8a22f0e8348c9
hampussle <hlinderstam@gmail.com>
2020-09-01 11:00:16 +0200

    FlagApp

--- Flag/FlagApp/Program.cs
+++ Flag/FlagApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FlagApp
 {
@@ -8,6 +9,46 @@ namespace FlagApp
         {
             //Skapa en applikation som ritar ut svenska flaggan
             //Använd metoder för att göra utritningen generell
+
+            //▓▓
+
+            //░░
+
+            
+            static void DrawFlag()
+            {
+                for (var y = 0; y < 4; y++)
+                {
+                    Console.WriteLine();
+                    for (var x = 0; x < 16; x++)
+                    {
+                        if (x == 5 || x == 6)
+                        {
+                            Console.Write("░░");
+                            continue;
+                        }
+                        Console.Write("▓▓");
+                    }
+                }
+            }
+
+            
+            static void DrawMidFlag()
+            {
+                for (var y = 0; y < 2; y++)
+                {
+                    Console.WriteLine();
+                    for (var x = 0; x < 16; x++)
+                    {
+                        Console.Write("░░");
+                    }
+                }
+            }
+
+            DrawFlag();
+            DrawMidFlag();
+            DrawFlag();
+            Console.ReadLine();
         }
     }
 }
