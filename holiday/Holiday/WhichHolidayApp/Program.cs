a6a98c441f138085af794e3c93257e72e4c3b24e
Andreas Brunnström <best_i25@hotmail.com>
2020-09-01 16:05:06 +0200

    Metoder i metoder

--- holiday/Holiday/WhichHolidayApp/Program.cs
+++ holiday/Holiday/WhichHolidayApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.PortableExecutable;
 
 namespace WhichHolidayApp
 {
@@ -8,6 +9,13 @@ namespace WhichHolidayApp
         {
             // Implementera följande flödesschema med metoder och användarinmatning
             // https://pbs.twimg.com/media/EQup9bwXUAEK5a_?format=jpg&name=large
+
+            // Man skulle kunna använda arrayer. 
+            // string [] frågor ?
+            // string [,] matchande fråga ger ett svar.
+
+
+            Sill.Run();
         }
     }
 }
---
+++ holiday/Holiday/WhichHolidayApp/Sill.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhichHolidayApp
+{
+    class Sill
+    {
+        public static void Run() // Gren 0
+        {          
+            string input = "";
+
+            while (input != "n" && input != "j")
+                {
+                Console.WriteLine("Äter man sill ? svara med ett n eller j");
+                input = Console.ReadLine();
+                }
+
+            if (input == "n")
+            {
+                FikaViktigt();
+            }
+            else
+            {
+                DrickaMust();
+            }
+        }
+
+        static void DrickaMust() // Gren 1
+        {
+            Console.WriteLine("Dricker man must ? svara med ett n eller j");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (input != "n" && input != "j");
+
+            if (input == "n")
+            {
+                Console.WriteLine("Det är midsommar!");
+            }
+            else
+            {
+                SeTv();
+            }
+        }
+
+        static void FikaViktigt() // Gren 1
+        {
+            Console.WriteLine("Är fika viktigt ? svara med ett n eller j");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (input != "n" && input != "j");
+
+            if (input == "n")
+            {
+                // Anrop Importerat av köpman från USA
+            }
+            else
+            {
+                WhipCream();
+            }
+        }
+
+        static void SeTv() // Gren 2
+        {
+            Console.WriteLine("Tittar man på tv kl 15:00 ? svara med ett n eller j");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (input != "n" && input != "j");
+
+            if (input == "n")
+            {
+                Console.WriteLine("Det är påsk!");
+            }
+            else
+            {
+                Console.WriteLine("Det är jul!");
+            }
+        }
+
+        static void WhipCream() // Gren 2
+        {
+            Console.WriteLine("Vispgrädde ? svara med ett n eller j");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (input != "n" && input != "j");
+
+            if (input == "n")
+            {
+                Console.WriteLine("Det är kanelbullens dag!");
+            }
+            else
+            {
+                SyltVal();
+            }
+        }
+
+        static void SyltVal()
+        {
+            Console.WriteLine("Sylt ? svara med ett n eller j");
+            string input = "";
+            do
+            {
+                input = Console.ReadLine();
+            }
+            while (input != "n" && input != "j");
+
+            if (input == "n")
+            {
+                Console.WriteLine("Det är fettisdagen!");
+            }
+            else
+            {
+                Console.WriteLine("Det är våffeldagen!");
+            }
+        } // Gren 3
+    }
+}
