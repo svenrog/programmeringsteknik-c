73439be4fe5e2961dada38ceaa6ee242eeaf22bf
Andreas Brunnström <best_i25@hotmail.com>
2020-09-01 09:52:19 +0200

    Ver.1.0 draw successful. Missing: colors

--- flag/Flag/FlagApp/Program.cs
+++ flag/Flag/FlagApp/Program.cs
@@ -8,6 +8,47 @@ namespace FlagApp
         {
             // Skapa en applikation som ritar ut svenska flaggan
             // Använd metoder för att göra utritningen generell
+
+            static void DrawMix()
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    for (int y = 0; y < 30; y++)
+                    {                        
+                        if (y < 10)
+                        {
+                            Console.Write("-░░-");
+                        }
+                        else if (y > 10 && y < 16)
+                        {
+                            Console.Write("-▓▓-");
+                        }
+                        else
+                        {
+                            Console.Write("-░░-");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            static void DrawSingle()
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    for (int y = 0; y < 30; y++)
+                    {
+                        if (y < 30)
+                        {
+                            Console.Write("-▓▓-");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            DrawMix();
+            DrawSingle();
+            DrawMix();
         }
     }
 }
