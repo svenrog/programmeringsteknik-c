a1d9259eaf104ec9309017131f1950314baf8418
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-04 09:57:33 +0200

    NumbersApp

---
+++ Numbers/Numbers/Numbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Konvertera det inmatade värdena från strängar till siffror
+            // Resultatet skall presentera:
+            // Lägsta värdet
+            // Högsta värdet
+            // Medelvärde (snitt)
+            // Programmet skall vara felhanterat
+            // Felaktiga värden får inte påverka medelvärde, lägsta eller högsta.
+
+            Console.WriteLine("skriv ett antal nummmer separerade med semicol!");
+            var userInput = Console.ReadLine();
+            string[] userInputArray = userInput.Split(";", StringSplitOptions.RemoveEmptyEntries);
+            List<double> numberList = new List<double>();
+
+            foreach(string stringNumber in userInputArray)
+            {
+                if (Double.TryParse(stringNumber, out double doubleNumber)) numberList.Add(doubleNumber);
+            }
+            double numberMaxValue = numberList.Max();
+            double numberMinValue = numberList.Min();
+            double sumValues = 0;
+
+            foreach (double value in numberList) sumValues += value;
+            double numberMedianValue = sumValues / numberList.Count;
+
+            Console.WriteLine($"Lägsta värdet: {numberMinValue}");
+            Console.WriteLine($"Högsta värdet: {numberMaxValue}");
+            Console.WriteLine($"Medelvärdet: {numberMedianValue}");
+        }
+    }
+}
