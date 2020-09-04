000cddcb446c3d2a2fa5630e14b14af6fdf1d436
DESKTOP-4QQ7EF3\elsam <elsa.matteoni@hotmail.com>
2020-09-04 11:33:56 +0200

    Progress

--- numbers/Numbers/NumbersApp/Program.cs
+++ numbers/Numbers/NumbersApp/Program.cs
@@ -7,18 +7,11 @@ namespace NumbersApp
     {
         static void Main(string[] args)
         {
-            // Mata in ett antal värden
-            // Konvertera det inmatade värdena från strängar till siffror
-            // Resultatet skall presentera:
-            // Lägsta värdet
-            // Högsta värdet
-            // Medelvärde (snitt)
-            // Programmet skall vara felhanterat
-            // Felaktiga värden får inte påverka medelvärde, lägsta eller högsta.
+            
 
             Console.Write("Ange ett antal nummer separerade med kommatecken:");
-            //string userInput = Console.ReadLine();
-            string userInput = "2,4,6,3,7,1,5,ytyr";
+            string userInput = Console.ReadLine();
+            
             string[] userInputArray = userInput.Split(",", StringSplitOptions.RemoveEmptyEntries);
             int lowestValue = int.MaxValue;
             int highestValue = int.MinValue;
