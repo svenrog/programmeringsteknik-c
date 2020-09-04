9b1de5259a1a775184b36f2ae074f9c0acbf2429
Edgar Runnman <edgar.runnman@gmail.com>
2020-09-04 11:30:15 +0200

    NumbersApp next version

--- Numbers/Numbers/Numbers/Program.cs
+++ Numbers/Numbers/Numbers/Program.cs
@@ -22,19 +22,22 @@ namespace Numbers
             List<double> numberList = new List<double>();
 
             foreach(string stringNumber in userInputArray)
-            {
                 if (Double.TryParse(stringNumber, out double doubleNumber)) numberList.Add(doubleNumber);
+
+            if (numberList.Count > 0)
+            {
+                double numberMaxValue = numberList.Max();
+                double numberMinValue = numberList.Min();
+                double sumValues = 0;
+                foreach (double value in numberList)
+                    sumValues += value;
+                double numberMedianValue = sumValues / numberList.Count;
+
+                Console.WriteLine($"Lägsta värdet: {numberMinValue}");
+                Console.WriteLine($"Högsta värdet: {numberMaxValue}");
+                Console.WriteLine($"Medelvärdet: {numberMedianValue}");
             }
-            double numberMaxValue = numberList.Max();
-            double numberMinValue = numberList.Min();
-            double sumValues = 0;
-
-            foreach (double value in numberList) sumValues += value;
-            double numberMedianValue = sumValues / numberList.Count;
-
-            Console.WriteLine($"Lägsta värdet: {numberMinValue}");
-            Console.WriteLine($"Högsta värdet: {numberMaxValue}");
-            Console.WriteLine($"Medelvärdet: {numberMedianValue}");
+            else Console.WriteLine("inga av värden är nummer");
         }
     }
 }
