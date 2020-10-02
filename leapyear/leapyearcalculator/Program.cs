6c079ff7b5e802d93409a0a01b1f57fc39de154d
Svenrog <sven.erik@geta.no>
2020-10-02 16:05:45 +0200

    Leapyear hint.

--- leapyear/leapyearcalculator/Program.cs
+++ leapyear/leapyearcalculator/Program.cs
@@ -7,6 +7,8 @@ namespace leapyearcalculator
         static void Main(string[] args)
         {
             // Räkna ut hur många skottår som passerat mellan två inmatade värden.
+
+            // DateTime.IsLeapYear(year) är en metod man kan använda.
         }
     }
 }
