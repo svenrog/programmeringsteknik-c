9b731324fb73b164dfb908f8770db9d34ca63afa
Svenrog <sven.erik@geta.no>
2020-10-02 16:04:45 +0200

    Leapyear

---
+++ leapyear/leapyearcalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leapyearcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Räkna ut hur många skottår som passerat mellan två inmatade värden.
+        }
+    }
+}
