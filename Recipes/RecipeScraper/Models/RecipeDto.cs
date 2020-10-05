fd797bec32fe19a9d0af9f92585546ed9f2d61e8
Sven-Erik Jonsson <sven.erik@geta.no>
2020-10-05 16:16:27 +0200

    Recipes

---
+++ Recipes/RecipeScraper/Models/RecipeDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RecipeScraper.Models
+{
+    class RecipeDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public List<string> RecipeIngredient { get; set; }
+        public List<InstructionDto> RecipeInstructions { get; set; }
+    }
+}
