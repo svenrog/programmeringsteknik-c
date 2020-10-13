243256ab7ebe847447bc1cf794c481155dad225d
andreas brunnström <best_i25@hotmail.com>
2020-10-13 16:09:57 +0200

    EndOffDay

---
+++ brownfield/TimeSheet.Common/Models/PaymentModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace TimeSheet.Common.Models
+{
+    public class PaymentModel
+    {
+        public int HourLimit { get; set; }
+        public string Label { get; set; } = "time";
+        public decimal HourlyRate { get; set; }
+    }
+}
---
+++ brownfield/TimeSheet.Common/Models/TimeSheetEntryModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace TimeSheet.Common.Models
+{
+    public class TimeSheetEntryModel
+    {
+        public string WorkDone { get; set; }
+        public int HoursWorked { get; set; }
+    }
+}
