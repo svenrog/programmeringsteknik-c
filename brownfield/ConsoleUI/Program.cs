a677f7d715b7494d260c22ddeb2ee17f7e656551
yanrosen <275522688@qq.com>
2020-10-14 08:45:07 +0200

    Merge

--- brownfield/ConsoleUI/Program.cs
+++ brownfield/ConsoleUI/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 // Please note - THIS IS A BAD APPLICATION - DO NOT REPLICATE WHAT IT DOES
 // This application was designed to simulate a poorly-built application that
 // you need to support. Do not follow any of these practices. This is for 
-// demonstration purposes only. You have been warned.
+// demonstration purposes only. You have been warned..
 namespace ConsoleUI
 {
     class Program
